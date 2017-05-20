b2032deefbe87ea1a340eeeef117a77233f105a4
Will Larson <lethain@gmail.com>
2017-05-20 10:04:11 -0700

    Add README, filtering running.

---
+++ README.md
@@ -0,0 +1,48 @@
+
+
+
+There are a handful of ways to configure which deployments are eligible for fault injection.
+The recommended starting place is to add the `fault_injection.opt_in: "true"` annotation for
+deployments you want to opt-in.
+
+```
+kind: Deployment
+metadata:
+  name: gke-ci
+    annotations:
+        fault_injection.opt_in: "true"
+```
+
+You can do this for each deployment you want to be eligible.
+Conversely, you can also use the `--include-by-default` CLI flag
+which will default all deployments in except for those which specify
+`fault_injection.opt_out: "true"` or which are excluded via parameter
+passed to the `--ignore` flag which is a list of comma-separated namespaces
+which are ignored (default value is `kube-system`).
+
+Note that `opt_in` and `opt_out` are basically local overrides for global behavior:
+
+   # include no matter what
+   fault_injection.opt_in: "true"
+   
+   # exclude no matter what
+   fault_injection.opt_out: "true"
+
+Consequently, `opt_in: "false"` is *not* equivalent to `opt_out: "true"`.
+
+Once you've opted in the appropriate deployments, then you just need to run `fi.py`.
+The simplest approach is to run it locally, riding over `kubectl proxy`:
+
+    # start kubectl in another terminal or in background
+    kubectl proxy &
+
+    # install and run
+    git clone ...
+    virtualenv env
+    . ./env/bin/activate
+    pip install -r requirements.txt
+    python fi --loc http://localhost:8001
+
+You can also use the `Dockerfile` included in this repository
+to run it as a Kubernetes Cron Job, where it'll use the container's
+serviceaccount tokens to authenticate to `http://kubernetes`.
--- fi.py
+++ fi.py
@@ -1,10 +1,12 @@
 """
 Proof of concept around simple fault injection for Kubernetes.
 """
+import argparse
+import pprint
 import requests
 
 
-def KClient(object):
+class KClient(object):
     "Kubernetes client."
     default_cert_path = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
     default_token_path = '/var/run/secrets/kubernetes.io/serviceaccount/token'
@@ -27,7 +29,7 @@ def KClient(object):
     def request(self, method, path, *args, **kwargs):
         "Perform request."
         url = "%s%s" % (self.loc, path)
-        return self.session().request(method, url, *args, **kwarg)
+        return self.session().request(method, url, *args, **kwargs)
 
     def get(self, path, *args, **kwargs):
         "Perform GET."
@@ -38,46 +40,56 @@ def KClient(object):
         return self.request('patch', path, *args, **kwargs)
 
 
-def deployments(cli, include_by_default=False, ignore_namespaces=None):
+def deployments(kc, include_by_default=False, ignore_namespaces=None):
     """
     Build list of deployments to consider. This is done by scanning all
-    deployments and checking for these annotations:
-
-        metadata:
-          annotations:
-            # if you want to opt in
-            gke_fault_injection/opt_in: true
-            # if you want to opt out
-            gke_fault_injection/opt_in: false
+    deployments and checking the `fault_injection.opt_in` annotation.
 
     If ignore_by_default is true, then ignore_namespaces is not used,
-    and only deployments with explicit `opt_in: true` flags will be considered.
+    and only deployments with explicit `opt_in: true` or `opt_out: true`
+    flags will be considered.
 
     `ignore_namespaces` will cause the listed namespaces to behave as if
     `include_by_default = False`, even when the default behavior is `include_by_default = True`.
     That also means, that `ignore_namespaces` doesn't have any behavior if `include_by_default = False`.
     """
-    deps = cli.get("/apis/extensions/v1beta1/deployments").json()['items']
+    deps = kc.get("/apis/extensions/v1beta1/deployments").json()['items']
 
     enabled = []
     for dep in deps:
         meta = dep.get('metadata', {})
         name = meta.get('name', 'missing-name')
         namespace = meta.get('namespace', 'missing-namespace')
-        annotations = dep['metadata']'annotations']
-        opt_in = annotations.get('gke_fault_injection/opt_in', False)
-        if namespace in ignore:
+        annotations = dep['metadata']['annotations']
+        opt_in = annotations.get('fault_injection.opt_in', None)
+        opt_out = annotations.get('fault_injection.opt_out', None)
+
+        if namespace in ignore_namespaces:
             continue
+        if opt_out:
+            continue        
         if (not include_by_default) and not opt_in:
             continue
         enabled.append(dep)
     return enabled
 
+"""
+    metadata:
+      labels:
+        app: gke-ci
+      annotations:
+        fault_injection.opt_in: "true"
+"""
+
 
 def inject_faults(loc, include_by_default, ignore_namespaces):
     kc = KClient(loc)
     for dep in deployments(kc, include_by_default, ignore_namespaces):
-        pass
+        meta = dep.get('metadata', {})
+        annotations = meta.get('annotations', {})
+        del annotations['kubectl.kubernetes.io/last-applied-configuration']
+        pprint.pprint(meta)
+        print ''
 
 
 
@@ -87,10 +99,10 @@ def main():
     p.add_argument('--loc', default='https://kubernetes', help='location to access Kubernetes API')
     p.add_argument('--include-by-default', default=False, action='store_true', help='opt all deploys in by default')
     p.add_argument('--ignore', default='kube-system', help='csv of namespaces to ignore')
-    
-            
-    parsed = p.parse_args()
-    inject_faults(parsed.loc, args.ignore.split(','))
+
+
+    args = p.parse_args()
+    inject_faults(args.loc, args.include_by_default, args.ignore.split(','))
 
 
 if __name__ == '__main__':
